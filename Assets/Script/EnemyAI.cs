7637cddc9b0e353f274787f2ad42efb9da3815b1
Trollface8D <82329632+Trollface8D@users.noreply.github.com>
2022-11-19 22:13:02 +0700

    Upload all file to github

---
+++ Assets/Script/EnemyAI.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public float startspeed = 20f;
+    public float chasespeed = 40f;
+    public float walkSpeed = 20f;
+    [HideInInspector]
+
+    public bool mustpatrol;
+
+    public Rigidbody2D rb;
+
+    private float direction = 1f;
+
+    public Transform groundCheckPos;
+    public Transform WallCheck;
+    public LayerMask groundlayer;
+    public LayerMask Playerlayer;
+
+    public Transform Player;
+    public Transform Enemy;
+
+    public float filprange = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mustpatrol = true;
+        walkSpeed = startspeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mustpatrol)
+        {
+            rb.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime * 10, rb.velocity.y);
+        }
+
+        //AI follow player
+        if (Mathf.Abs(Player.position.x - Enemy.position.x) <filprange)
+        {
+            
+        }
+        else if (!IsPlayer())
+        {
+            //AI Patrol player
+            if (IsWall() || IsFilp())
+            {
+                flip();
+            }
+            //
+            if (walkSpeed > 0f)
+            {
+                walkSpeed = startspeed;
+            }
+            else
+            {
+                walkSpeed = -startspeed;
+            }
+        }
+        else if (IsPlayer())
+        {
+            //Debug.Log("Hitting");
+            if (walkSpeed > 0f)
+            {
+                walkSpeed = chasespeed;
+                if ((Player.position.x - Enemy.position.x) < 0)
+                {
+                    flip();
+                }
+                
+            }
+            else if(walkSpeed < 0f)
+            {
+                walkSpeed = -chasespeed;
+                if ((Player.position.x - Enemy.position.x) > 0)
+                {
+                    flip();
+                }
+            }
+            if (IsWall() || IsFilp())
+            {
+                flip();
+            }
+        }
+              
+    }
+    //end follow player
+    void flip()
+    {
+        mustpatrol = false;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        //direction *= -1;
+        walkSpeed *= -1;
+        mustpatrol = true;
+    }
+    private bool IsWall()
+    {
+        return Physics2D.OverlapCircle(WallCheck.position, 0.2f, groundlayer);
+    }
+
+    private bool IsFilp()
+    {
+        return !Physics2D.OverlapCircle(groundCheckPos.position, 0.2f, groundlayer);
+    }
+    private bool IsPlayer()
+    {
+        return Physics2D.OverlapBox(new Vector2(Enemy.position.x, Enemy.position.y + 1f), new Vector2(20f,2f),0, Playerlayer);
+        //return Physics2D.OverlapCircle(Enemy.position, 3f, Playerlayer);
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float horizontal;
+    public float speed = 8f;
+    public float jumpingPower = 16f;
+    private bool isFacingRight = true;
+    private bool doublejump = true;
+
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Transform LeftWallCheck;
+    [SerializeField] private Transform RightWallCheck;
+    [SerializeField] private LayerMask groundLayer;
+ 
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (IsGrounded())
+        {
+            doublejump = true;
+        }
+        
+
+        if (Input.GetButtonDown("Jump")&& (IsGrounded()))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        //mutiple jump
+        else if(Input.GetButtonDown("Jump") && (doublejump  )) //double jump : remove "Iswall()"
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            doublejump = false;
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        filp();
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+    private bool IsWall()
+    {
+        if (Physics2D.OverlapCircle(LeftWallCheck.position, 0.2f, groundLayer) || Physics2D.OverlapCircle(RightWallCheck.position, 0.2f, groundLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+    private void filp()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
